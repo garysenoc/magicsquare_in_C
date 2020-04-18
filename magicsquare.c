e176ad06f7bbfb96f42ef8d3ef8250687be41ccc
Gary Lloyd M. Senoc <62071560+garysenoc@users.noreply.github.com>
2020-04-18 17:41:09 +0800

    Add files via upload

---
+++ magicsquare.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main(){
+	
+	int num=1,j,i,start=1,x=0,y;
+	
+	printf("Enter size of magic square (Only odd number): ");
+	scanf("%d",&num);
+	
+	int arr[num][num];
+	
+	for(i=0;i<num;i++)
+		for(j=0;j<num;j++)
+			arr[i][j] = 0;
+			
+	arr[0][(num/2)] = start++;
+	y = (num/2);
+	
+	while(start <= num * num){
+	y++;
+	x--;
+	
+	if(x==-1)
+		x=num-1;
+	if(y==num)
+		y = 0;
+
+	if(arr[x][y] != 0){
+		x+=2;
+		y--;
+	}
+	if( x>num && y <0){
+		y = num-1;
+		x =  x - num;
+	}	
+		arr[x][y] = start++;
+	}
+	
+	for(i=0;i<num;i++){
+		for(j=0;j<num;j++)
+			printf("%5d ",arr[i][j]);
+		printf("\n");
+	}			
+	return 0;
+}
